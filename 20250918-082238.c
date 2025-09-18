c6e08328d77e46ef2c4bae12d460e960699f951f
Resiato9 <lydia.26275@students.kyu.ac.ke>
2025-09-18 10:34:23 +0300

    Add files via upload

---
+++ 20250918-082238.c
@@ -0,0 +1,20 @@
+//simple C program 
+/*
+Name:Lydia Resiato 
+Reg:CT100/G/26275/25
+Description:hello c
+*/
+
+#include<stdio.h>//pre-proccessor 
+
+//main function 
+int main(){
+printf("hello c:\n");
+scanf("enter my height:\n");
+printf("118.6\n");
+scanf("enter my phone number:\n");
+printf("0790885756:\n");
+scanf("enter my weight:\n");
+printf("55.8:\n");
+return 0;
+}
