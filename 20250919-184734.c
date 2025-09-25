1ab3ac3a73b3db5610b9859c2ef7ff23953b2e6b
Resiato9 <lydia.26275@students.kyu.ac.ke>
2025-09-25 10:56:48 +0300

    Add files via upload

---
+++ 20250919-184734.c
@@ -0,0 +1,23 @@
+//simple C program (hello c)
+/*
+Name:Lydia Resiato 
+Reg:CT100/G/26275/25
+description: bachelor of science in information technology 
+*/
+
+#include<stdio.h>//pre-proccessor 
+
+//main function 
+int main(){
+printf("Hello c:\n");
+printf("Bachelor of science in information technology;\n");
+printf("semester one units:\n");
+printf("analogue electronics:\n");
+printf("fundamentals of ICT:\n");
+printf("essential of accounting:\n");
+printf("introduction to programming and algorithms:\n");
+printf("computer systems and organization:\n");
+printf("communication skills:\n");
+printf("computer systems and organization:\n");
+return 0;
+}
